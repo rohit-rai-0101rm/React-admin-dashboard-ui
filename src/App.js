cb664ded72f1543f2e4c8f65527f53e47e88e795
rohit rai <rohitrai.dev@gmail.com>
2021-12-01 09:21:46 +0530

    home page in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
 import './app.css'
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        other pages
-
+        <Home/>
       </div>
     </div>
   );
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './chart.css'
+const Chart = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Chart
