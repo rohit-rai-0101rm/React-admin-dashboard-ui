5e5b57f0238664f7330a0ba79c58a627411b90d9
rohit rai <rohitrai.dev@gmail.com>
2021-12-02 06:05:29 +0530

    line chart rendered

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './chart.css'
-const Chart = () => {
+import {
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+  } from "recharts";
+  
+const Chart = ({title,data,dataKey,grid}) => {
     return (
-        <div>
-            
-        </div>
+        <div className="chart">
+        <h3 className="chartTitle">{title}</h3>
+        <ResponsiveContainer width="100%" aspect={4 / 1}>
+          <LineChart data={data}>
+            <XAxis dataKey="name" stroke="#5550bd" />
+            <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+            <Tooltip />
+            {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Chart from '../../components/Chart/Chart'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import './home.css'
+import { userData } from '../../dummyData'
+
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+        <div className="homeWidget"></div>
+        
         </div>
     )
 }
